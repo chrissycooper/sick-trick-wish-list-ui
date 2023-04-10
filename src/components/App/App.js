7df21dc936e09fe4ecb6e111f0d9cd9cfc37ffce
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 10:34:11 -0500

    feat: Add basic Tricks component infrastructure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import { Component } from 'react';
 import './App.css';
+import Tricks from '../Tricks/Tricks';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      tricks: {}
+      tricks: []
     }
   }
 
   componentDidMount(){
-    
+
   }
 
 
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Tricks />
       </div>
     );
   }
---
+++ src/components/Tricks/Tricks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Tricks.css';
+
+
+const Tricks = () => {
+
+  return (
+    <div className='tricks-container'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Tricks;
