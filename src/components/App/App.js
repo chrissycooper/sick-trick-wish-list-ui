06a3145b4a08697c1cf7b8d9b54cf2ff02d60a6f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 10:25:17 -0500

    feat: Complete iteration 0

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,18 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tricks: {}
+    }
+  }
+
+  componentDidMount(){
+    
+  }
+
+
   render() {
     return (
       <div className="App">
