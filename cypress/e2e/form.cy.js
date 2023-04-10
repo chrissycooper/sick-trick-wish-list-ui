819768e454b4e58572ed5751de58939f83609c0f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-10 12:54:24 -0500

    test: Add test for form functionality

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,4 +1,4 @@
-describe('empty spec', () => {
+describe('when data is put into the form, the value is reflected in that form input.', () => {
   beforeEach(()=> {
     cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {
       statusCode: 200,
@@ -18,8 +18,19 @@ describe('empty spec', () => {
           "id": "5"
           }
         ]
-      
     })
     .visit('http://localhost:3000/')
   })
+
+  it('Should display the form', () => {
+    cy.get('[placeholder="Name of Trick"]')
+    cy.get('button')
+  })
+
+  it('Should reflect the value', () => {
+    cy.get('[placeholder="Name of Trick"]')
+    .type('Shove-it')
+    .should('have.value', 'Shove-it')
+  })
+
 })
